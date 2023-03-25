76d909b4d520b91d9c6cdf3b3f6286bb2ff729ef
zacharydgomez <zachary.d.gomez@gmail.com>
2023-03-25 11:21:58 -0500

    fixed the linter

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 // import './styles.css';
 
-
-
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(<App tab="home" />);
