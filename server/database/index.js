fa804d380aaa0424557c6a01a1e2a54fc877a44e
zacharydgomez <zachary.d.gomez@gmail.com>
2023-03-24 19:39:02 -0500

    sequalize connecting to server

---
+++ server/database/index.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+// // Option 1: Passing a connection URI
+// const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+
+// // Option 2: Passing parameters separately (sqlite)
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'path/to/database.sqlite'
+// });
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('test', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+ sequelize.authenticate().then(()=>{console.log("connected")});
+module.exports.sequelize = sequelize
