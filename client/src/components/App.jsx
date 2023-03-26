a6e6b445cb0f54f0053278335d1d26495bede3e1
tweezerchef <ltomblock@gmail.com>
2023-03-25 19:23:48 -0500

    newPullFromGageDeletedBranchLocally

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import Story2 from './Story2.jsx';
+import VideoList from './VideoList.jsx'
 
 class App extends React.Component {
   constructor(props) {
--- client/src/components/Story2.jsx
+++ client/src/components/Story2.jsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
+import axios from 'axios';
 import DestC from './DestC.jsx';
 import OriginC from './OriginC.jsx';
 
@@ -14,6 +15,8 @@ function Story2() {
   const changeOrigin = (origin) => setOrigin(origin);
   const [destC, setDest] = React.useState('');
   const changeDest = (destination) => setDest(destination);
+  const findUnique = () => {
+  };
   return (
     <>
       <CssBaseline />
@@ -24,7 +27,7 @@ function Story2() {
         >
           {' '}
           <OriginC changeOrigin={changeOrigin} sx={{ flex: 1, marginLeft: '125px' }} />
-          <Button variant="contained" endIcon={<FlightTakeoffIcon />} size="large" sx={{ width: 170, height: 90 }}> LFG </Button>
+          <Button variant="contained" endIcon={<FlightTakeoffIcon />} size="large" onClick={findUnique} sx={{ width: 170, height: 90 }}> LFG </Button>
           <DestC changeDest={changeDest} sx={{ flex: 1, marginLeft: '125px' }} />
 
         </Box>
