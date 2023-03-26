fe7f74178dcd722b4a80a4038b196d372b577214
tweezerchef <ltomblock@gmail.com>
2023-03-26 16:35:46 -0500

    movieDataRendering

--- client/src/components/Story2/UniqueTable.jsx
+++ client/src/components/Story2/UniqueTable.jsx
@@ -6,38 +6,36 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Button } from '@mui/material';
+import { fakeUniqueData } from '../../NetFlixCountries';
 
+const firstFake = fakeUniqueData[0];
 function createData(
   name,
-  calories,
-  fat,
-  carbs,
-  protein,
+  image,
+  trailer,
+  addToWatchList,
 ) {
   return {
-    name, calories, fat, carbs, protein,
+    name,
+    image,
+    trailer,
+    addToWatchList,
   };
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+const rows = fakeUniqueData.map((movie) => createData(movie.title, movie.img, movie));
 
 export default function BasicTable() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 400 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Unique Programs</TableCell>
+            <TableCell align="right">Image</TableCell>
+            <TableCell align="right">Watch Trailer</TableCell>
+            <TableCell align="right">Add To WatchList</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -49,10 +47,19 @@ export default function BasicTable() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">
+                <img src={row.image} alt={row.name} width="100" />
+              </TableCell>
+              <TableCell align="right">
+                <Button variant="contained" color="primary">
+                  Watch Trailer
+                </Button>
+              </TableCell>
+              <TableCell align="right">
+                <Button variant="contained" color="secondary">
+                  Add To WatchList
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
