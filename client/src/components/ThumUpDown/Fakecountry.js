c529e56643809cdcd4cae71ad10657b0955c6750
gagecantrelle <gagecantrelle1899@gmail.com>
2023-03-27 15:07:35 -0500

    added some markers to map

---
+++ client/src/components/ThumUpDown/Fakecountry.js
@@ -0,0 +1,77 @@
+//space out ones are not complete
+const locations = [
+  { markerOffset: -25, name: "Belgium", coordinates: [5, 51.4897]},
+  { markerOffset: -25, name: "Australia", coordinates: [130.8825, -22.7942]},
+  { markerOffset: -25, name: "Argentina", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Brazil", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "Canada", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Colombia", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Czech Republic", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "France", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Germany", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Greece", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "Hong Kong", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Hungary", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Iceland", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "India", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Israel", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Italy", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "Japan", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Lithuania", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Malaysia", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "Mexico", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Netherlands", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Philippines", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Poland", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "Portugal", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Romania", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Russia", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "Singapore", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Slovakia", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "South Africa", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "South Korea", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "South Korea", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Sweden", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "Switzerland", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "Thailand", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "Turkey", coordinates: [5, 51.4897]},
+
+  { markerOffset: -25, name: "Ukraine", coordinates: [130.8825, -22.7942]},
+
+  { markerOffset: -25, name: "United Kingdom", coordinates: [-64.6693, -33.4489]},
+
+  { markerOffset: -25, name: "United States", coordinates: [-64.6693, -33.4489]},
+]
+export default locations;
