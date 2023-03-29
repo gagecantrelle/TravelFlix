bd2dc4b6de1ee905d5ca4f77bd1e253535391ce5
zacharydgomez <zachary.d.gomez@gmail.com>
2023-03-29 12:23:45 -0500

    changed how userfeed and mediainfo render

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -16,24 +16,10 @@ class App extends React.Component {
     super(props);
     this.state = {
       // eslint-disable-next-line react/no-unused-state
-      selectedMovie: {
-        title: 'The Goat and Her Three Kids',
-        img: 'https://occ-0-2758-3467.1.nflxso.net/dnm/api/v6/WNk1mr9x_Cd_2itp6pUM7-lXMJg/AAAABcAhg7Ed9mUM0OwBbe0DMWKmi9W4HcbSYhpju4xeMRr1WODB1iVTACz8_XS7JP32HKU5UmHoy0a-5hh70yVj9eYehl1jSatX3g.jpg?r=852',
-        title_type: 'movie',
-        netflix_id: 81677719,
-        synopsis: 'A determined widow must protect her three children from a bloodthirsty neighbor bent on revenge in this gruesome spin on the beloved fairy tale.',
-        rating: '9.8',
-        year: '2022',
-        runtime: '4972',
-        imdb_id: 'tt9243412',
-        poster: 'https://m.media-amazon.com/images/M/MV5BNWUxOGM3ZWUtOTUzZS00ODQwLTlkMmUtZjhkYTNhM2JkNGUxXkEyXkFqcGdeQXVyNTI0MjQyMDU@._V1_SX300.jpg',
-        top250: 0,
-        top250tv: 0,
-        title_date: '2023-03-24',
-      },
+      selectedMovie: null,
       userName: 'fred',
       userObject: {},
-      activityFeedUsers: {},
+      activityFeedUsers: null,
       darkTheme: createTheme({
         palette: {
           mode: 'dark',
@@ -88,15 +74,14 @@ class App extends React.Component {
 
   render() {
     const { userName, selectedMovie, darkTheme, activityFeedUsers } = this.state;
-    if (activityFeedUsers === null) {
-      return <div>Loading...</div>;
-    }
+  
     return (
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
 
         <div>
-          <UserFeed activityFeedUsers={activityFeedUsers}/>
+          {activityFeedUsers && <UserFeed activityFeedUsers={activityFeedUsers} />}
+          {/* <UserFeed activityFeedUsers={activityFeedUsers} /> */}
           <Button variant="contained">Hello World</Button>
           {/* {this.state.userObject} */}
           <DarkModeSwitch
@@ -106,7 +91,7 @@ class App extends React.Component {
           
           <Story2 changeMovie={this.changeMovie} userName={userName} />
 
-          <MediaInfo selectedMovie={selectedMovie} />
+          {selectedMovie && <MediaInfo selectedMovie={selectedMovie} />}
 
         </div>
       </ThemeProvider>
