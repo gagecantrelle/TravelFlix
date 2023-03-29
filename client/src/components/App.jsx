6e4adbf7d078d5d36396b3730cead1c75cc84890
zacharydgomez <zachary.d.gomez@gmail.com>
2023-03-29 16:01:47 -0500

    resolving pull request

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,9 +4,10 @@ import axios from 'axios';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Story2 from './Story2/Story2.jsx';
+import UserFeed from  './Story6/UserFeed.jsx'
 import DarkModeSwitch from './DarkModeSwitch.jsx';
 import MediaInfo from './Story3/MediaInfo.jsx';
-
+import Button from '@mui/material/Button';
 // import UserFeed from './Story6/UserFeed.jsx';
 
 class App extends React.Component {
@@ -89,7 +90,7 @@ class App extends React.Component {
           {selectedMovie && <MediaInfo selectedMovie={selectedMovie} />}
 
         </div>
-      </div>
+      </ThemeProvider>
     );
   }
 }
--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { YOUTUBE_KEY } = process.env;
 const { UNOGD_KEY } = process.env;
-console.log(YOUTUBE_KEY + "hi")
+console.log(UNOGD_KEY + "hi")
 module.exports = {
   YOUTUBE_KEY, UNOGD_KEY,
 };
