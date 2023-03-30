775df4677ddaa964dca529f3feb7942abe5a45d2
zacharydgomez <zachary.d.gomez@gmail.com>
2023-03-30 10:41:17 -0500

    fixed the video list part

--- client/src/components/Story6/UserFeed.jsx
+++ client/src/components/Story6/UserFeed.jsx
@@ -1,43 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import './UserFeed.css'; 
 
 function UserFeed(props) {
   const { activityFeedUsers } = props;
-  const [userId, setUserId] = useState(Object.keys(activityFeedUsers)[0]);
+  const [index, setIndex] = useState(0);
 
   function handleClick(event) {
     console.log(`Clicked ${event}`);
   }
 
   useEffect(() => {
-    const userCount = Object.keys(activityFeedUsers).length;
     const intervalId = setInterval(() => {
-      setUserId((prevUserId) => {
-        const userIds = Object.keys(activityFeedUsers);
-        const currentIndex = userIds.indexOf(prevUserId);
-        const nextIndex = (currentIndex + 1) % userCount;
-        return userIds[nextIndex];
-      });
+      setIndex((index) => (index + 1) % activityFeedUsers.length);
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, [activityFeedUsers]);
+  }, [activityFeedUsers.length]);
 
-  const user = activityFeedUsers[userId];
+  function iterateMostRecentArray(dataObject) {
+    const keys = Object.keys(dataObject);
+    keys.sort((a, b) => b - a);
+    const mostRecentKey = keys[0];
+    const mostRecentArray = dataObject[mostRecentKey];
+    return mostRecentArray;
+  }
 
-
+  const user = activityFeedUsers[index];
+  const mostRecentMovies = iterateMostRecentArray(user.movieList);
 
   return (
-    <Paper sx={{ p: 2, width: 300, height: 150 }}>
+    <Paper sx={{ p: 2, width: 300, height: 300 }}>
       <Typography variant="h6" gutterBottom>
         Activity Feed
       </Typography>
-      <Typography variant="body1" onClick={() => console.log(user.userName)}>
-        {user && user.userName}
-      </Typography>
-     
+      {/* Add a div to wrap the user name */}
+      <div className="username-container">
+        {/* Add a CSS class to animate the transition */}
+        <Typography className="username" variant="body1" onClick={() => console.log(user.userName)}>
+          {user && user.userName}
+        </Typography>
+      </div>
+      {mostRecentMovies && mostRecentMovies.map((movie, index) => (
+        <Typography key={index} variant="body2" onClick={() => handleClick(movie)}>
+          {movie}
+        </Typography>
+      ))}
     </Paper>
   );
 }
-export default UserFeed
+
+export default UserFeed;
