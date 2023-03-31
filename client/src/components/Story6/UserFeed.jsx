29902b7ff3bbe8a1ae75230a6571b78a908c58ee
zacharydgomez <zachary.d.gomez@gmail.com>
2023-03-31 13:17:59 -0500

    medialist is rendering

--- client/src/components/Story6/UserFeed.jsx
+++ client/src/components/Story6/UserFeed.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-// import './UserFeed.css';
 import MediaInfo from '../Story3/MediaInfo.jsx';
 
 function UserFeed(props) {
   const { activityFeedUsers } = props;
   const [index, setIndex] = useState(0);
-  function handleClick(event) {
-    console.log(`Clicked ${event}`);
+  const [mediainfo, setMediaInfo] = useState(null);
+
+  function handleClick(movie) {
+    setMediaInfo(movie);
   }
 
   useEffect(() => {
@@ -18,44 +19,36 @@ function UserFeed(props) {
 
     return () => clearInterval(intervalId);
   }, [activityFeedUsers.length]);
-  // error
+
+  const user = activityFeedUsers[index];
+  const userMovieList = iterateMostRecentArray(user.movieList);
+  const mostRecentMovies = userMovieList.slice(0, 5);
+
   function iterateMostRecentArray(dataObject) {
     const keys = Object.keys(dataObject);
     keys.sort((a, b) => b - a);
     const mostRecentKey = keys[0];
-    //
     const mostRecentArray = dataObject[mostRecentKey];
     return mostRecentArray;
   }
-
-  const user = activityFeedUsers[index]; // 0Obj
-  // error
-  // resolve when data returns
-  const userMovieList = iterateMostRecentArray(user.movieList);
-
-  const mostRecentMovies = userMovieList.slice(0, 5);// movielist
-  console.log(mostRecentMovies);
 
   return (
     <Paper sx={{ p: 2, width: 300, height: 300 }}>
       <Typography variant="h6" gutterBottom>
         Activity Feed
       </Typography>
-      {/* Add a div to wrap the user name */}
       <div className="username-container">
-        {/* Add a CSS class to animate the transition */}
         <Typography className="username" variant="body1" onClick={() => console.log(user.userName)}>
           {user && user.userName}
         </Typography>
       </div>
       {mostRecentMovies && mostRecentMovies.map((movie, index) => (
-        <Typography key={index} variant="body2" onClick={() => handleClick(movie.name)}>
+        <Typography key={index} variant="body2" onClick={() => handleClick(movie)}>
           {movie.name}
         </Typography>
       ))}
-
+      {mediainfo && <MediaInfo selectedMovie={mediainfo.trailer} />}
     </Paper>
-
   );
 }
 
