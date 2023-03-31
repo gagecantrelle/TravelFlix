8731caf6cc6cc3142627f1e46b8aea9ade2f5d73
tweezerchef <ltomblock@gmail.com>
2023-03-30 21:11:02 -0500

    InterActiveWatchList

--- client/src/components/Story2/WatchList.jsx
+++ client/src/components/Story2/WatchList.jsx
@@ -1,6 +1,14 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState, useEffect } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Checkbox from '@mui/material/Checkbox';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 
 function WatchList(props) {
   const { userObject, keyCode, buttonClicked } = props;
@@ -18,6 +26,34 @@ function WatchList(props) {
     }
   }, [buttonClicked]);
 
+  const [checked, setChecked] = React.useState([1]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+  const handleDeleteSelected = () => {
+    // Update the movieList and remove the selected movies
+    const updatedMovieList = {
+      ...movieList,
+      [keyCode]: movieList[keyCode].filter((_, index) => !checked.includes(index)),
+    };
+
+    // Update the movieList
+    setMovieList(updatedMovieList);
+
+    // Reset the checked state
+    setChecked([]);
+  };
+
   // open new window and play netflix
 
   const openInNewTab = (url) => {
@@ -29,19 +65,48 @@ function WatchList(props) {
 
   if (movieList[keyCode] && Array.isArray(movieList[keyCode]) && movieList[keyCode].length > 0) {
     return (
-      <div>
-        <h4>Click To Watch</h4>
-        <ul>
-          {movieList[keyCode].map((movie, i) => (
-            <li
-              key={i}
-              onClick={() => playOnNetflix(movie.trailer.netflix_id)}
+      <List dense sx={{ width: '100%', maxWidth: 400, bgcolor: 'background.paper' }}>
+        <ListItem>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleDeleteSelected}
+          >
+            Delete Selected
+          </Button>
+        </ListItem>
+        {movieList[keyCode].map((movie, value) => {
+          const labelId = `checkbox-list-secondary-label-${value}`;
+          return (
+            <ListItem
+              key={value}
+              secondaryAction={(
+                <Checkbox
+                  edge="end"
+                  onChange={handleToggle(value)}
+                  checked={checked.indexOf(value) !== -1}
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              )}
+              disablePadding
             >
-              {movie.name}
-            </li>
-          ))}
-        </ul>
-      </div>
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar
+                    alt={`Avatar n°${value + 1}`}
+                    src={movie.image}
+                  />
+                </ListItemAvatar>
+                <ListItemText
+                  id={labelId}
+                  primary={movie.name}
+                  onClick={() => playOnNetflix(movie.trailer.netflix_id)}
+                />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
     );
   }
 
