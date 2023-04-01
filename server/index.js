7900d06d0c9046b7cfae0544d8d23adbbf893a76
tweezerchef <ltomblock@gmail.com>
2023-04-01 17:09:36 -0500

    depenInstalled

--- server/index.js
+++ server/index.js
@@ -78,7 +78,7 @@ const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
 app.use(express.static(CLIENT_PATH));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const PORT = 8085;
+const PORT = 8090;
 
 // search for youTube Clip
 app.post('/search', (req, res) => {
